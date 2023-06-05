52d55dfaace16a9a8572833a5d2f12f6cf07e0e5
derPatecopa <mb89@me.com>
2023-06-05 09:40:14 +0200

    added functions for getting all  data for rover

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -10,73 +10,66 @@ let store = Immutable.Map({
 // add our markup to the page
 const root = document.getElementById("root");
 
+//function for updating the store object
 const updateStore = (state, newState) => {
   //updating store for Immutable
+  //mergin fetched newState into current object
+  //merged data is not immutable, therefore you can access it via regular dot noation
   store = state.merge(newState);
   render(root, store);
 };
 
+//renders content to index.html via App()
 const render = async (root, state) => {
   root.innerHTML = App(state);
 };
 
 // create content
-
 const App = (state) => {
-  return `<p>${state.getIn(['rovers', 0]).curiosity.latest_photos[0].rover.name}</p>`;
+  return `<p>${
+    state.getIn(["rovers", 0]).curiosity.latest_photos[0].rover.name
+  }</p>
+  <img src="${state.getIn(["rovers", 0]).curiosity.latest_photos[0].img_src}" width="500" height="600">`;
 };
 
 // listening for load event because page should load before any JS is called
 window.addEventListener("load", async () => {
+  //loading the data for store BEFORE the site is being rendered
   await Promise.all(
-    store.get('rovers').map(async (roverName) => {
+    store.get("rovers").map(async (roverName) => {
       const roverData = await getRoverInfo(roverName);
-      store = store.setIn(['rovers', roverName], roverData);
+      store = store.setIn(["rovers", roverName], roverData);
     })
   );
-  console.log(store.getIn(['rovers',0]))
-  render(root, store)
-}
-);
+  console.log(store.getIn(["rovers", 0]));
+  render(root, store);
+});
 
 // ------------------------------------------------------  COMPONENTS
 
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
 
-// Example of a pure function that renders infomation requested from the backend
 
 // ------------------------------------------------------  API CALLS
 
 // getting Rover Data from API
-
 const getRoverInfo = async (roverName) => {
-  const data = await fetch(`http://localhost:${PORT}/rovers/${roverName}`).then(
-    (res) => res.json()
-  )
-  .catch((err) => console.errror(err));
+  // fetching data from api in index.js
+  const data = await fetch(`http://localhost:${PORT}/rovers/${roverName}`)
+    .then((res) => res.json())
+    .catch((err) => console.errror(err));
 
-  //Update store with the new roverData
-  store = store.updateIn(['rovers'], (roversList)=> {
-      const index = roversList.indexOf(roverName);
-      const newData = {[roverName]:data};
+  //Update store with the new roverData using updateIn from immutable
+  store = store.updateIn(["rovers"], (roversList) => {
+    const index = roversList.indexOf(roverName);
+    const newData = { [roverName]: data };
 
-  //if rover exists in the List, replace it with the updated version
-  if (index >= 0) {
-      return roversList.set(index,newData);
-  }
+    //if rover exists in the List, replace it with the updated version
+    if (index >= 0) {
+      return roversList.set(index, newData);
+    }
 
-  //if rover does not exist yet, append it to the list
-  return roversList.push(newData);
+    //if rover does not exist yet, append it to the list
+    return roversList.push(newData);
   });
   return data;
 };
-
-// getRoverInfo(store.get('rovers').get(0)).then(() => {
-//   console.log(store.getIn(['rovers', 0]).curiosity.latest_photos[0].id);
-// });
-
-
-
-
-
-
--- project/src/server/index.js
+++ project/src/server/index.js
@@ -49,17 +49,5 @@ app.get("/rovers/:rover_name", async (req, res) => {
   }
 });
 
-// app.get('/rovers/:rover_name/:max_sol/latest_photos', async (req,res) => {
-//     const {rover_name} = req.params;
-//     const {max_sol} = req.params;
-//     try {
-//         const latestPhotosResponse = await fetch (`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover_name}/photos?sol=${max_sol}&api_key=${process.env.API_KEY}`);
-//         const latestPhotos = await latestPhotosResponse.json();
-//         res.send(latestPhotos);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).send({error: 'Unable to retrieve latest photos'});
-//     }
-// });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
